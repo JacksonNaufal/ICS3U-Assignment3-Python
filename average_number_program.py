f27370ab903bcb5ec255138cdec4c19453dbd453
Jackson Naufal <Jackson.naufal@mths.ca>
2022-04-09 04:23:15 +0000

    Program

--- average_number_program.py
+++ average_number_program.py
@@ -12,35 +12,43 @@ def main():
     # input
     print("This calculator will calculate your three numbers!")
     print(
-        "Note: this calculator rounds to 2 decimals, and will not calculate numbers greater than 100"
+        "Note: this calculator rounds to 2 decimals, and will not calculate numbers greater than 100 or less than 0!"
     )
-    number_one = int(input("What is your first number?: "))
-    number_two = int(input("What is your second number?: "))
-    number_three = int(input("What is your third number?: "))
+    number_one_string = input("What is your first number?: ")
+    number_two_string = input("What is your second number?: ")
+    number_three_string = input("What is your third number?: ")
 
     # process & output
+    try:
 
-    if number_one >= 0 and number_one >= 100:
-        print(
-            "\nThe first number you inputted, which is {0} is too large, please try again!".format(
-                number_one
-            )
-        )
-        if number_two >= 0 and number_two >= 100:
-            print(
-                "\nThe second number you inputted, which is {0} is too large, please try again!".format(
-                    number_two
-                )
-            )
-            if number_three >= 0 and number_three >= 100:
+            number_one = int(number_one_string)
+            number_two = int(number_two_string)
+            number_three = int(number_three_string)
+
+            if number_one >= 0 and number_one >= 100:
                 print(
-                    "\nThe second number you inputted, which is {0} is too large, please try again!".format(
-                        number_three
+                    "\nThe first number you inputted, which is {0} is too large, please try again!".format(
+                        number_one
                     )
                 )
-    else:
-        print((number_one + number_two + number_three) / 3)
-
+                if number_two >= 0 and number_two >= 100:
+                    print(
+                        "\nThe second number you inputted, which is {0} is too large, please try again!".format(
+                        number_two
+                        )
+                    )
+                    if number_three >= 0 and number_three >= 100:
+                        print(
+                            "\nThe second number you inputted, which is {0} is too large, please try again!".format(
+                                number_three
+                            )
+                        )
+    
+            else:
+                print((number_one + number_two + number_three) / 3)
+            
+    except Exception:
+        print("Invalid Input")
     print("\nDone.")
 
 
