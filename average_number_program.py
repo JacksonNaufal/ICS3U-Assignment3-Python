b53ce6ffb93d5bbb7da045ae131d29d797e0c9d2
Jackson Naufal <Jackson.naufal@mths.ca>
2022-04-11 21:27:08 +0000

    Assignment 3

--- average_number_program.py
+++ average_number_program.py
@@ -5,43 +5,41 @@
 # This is a average number calculator program
 
 
-
 def main():
 
     # This is a average number calculator
 
     # input
     print("This calculator will calculate your three numbers!")
-    print(
-        "Note: this calculator rounds to 2 decimals, and will not calculate numbers greater than 100 or less than 0!"
-    )
+    print("Note: this calculator will not round!")
+    print("Note: Max number is 100, Min number is 0")
     number_one_string = input("What is your first number?: ")
     number_two_string = input("What is your second number?: ")
     number_three_string = input("What is your third number?: ")
-
     # process & output
     try:
 
-        number_one = int(number_one_string)
-        number_two = int(number_two_string)
-        number_three = int(number_three_string)
+        number_one = float(number_one_string)
+        number_two = float(number_two_string)
+        number_three = float(number_three_string)
 
-        if number_one >= 100: 
-            print("\nInvalid Number, please try again!!")
+        if number_one >= 100:
+            print("\nInvalid Number, please try again!! (Number 1)")
         elif number_two >= 100:
-            print("\nInvalid Number, please try again!")
+            print("\nInvalid Number, please try again!(Number 2)")
         elif number_three >= 100:
-            print("\nInvalid Number, please try again!")
-        elif number_one<= 0:
-            print("\nInvalid Number, please try again!")
-        elif number_two <= 0:
-            print("\nInvalid Number, please try again!")
-        elif number_three <= 0:
-            print("\nInvalid Number, please try again!")
-        
+            print("\nInvalid Number, please try again! (Number 3)")
+        elif number_one < 0:
+            print("\nInvalid Number, please try again! (Number 1)")
+        elif number_two < 0:
+            print("\nInvalid Number, please try again! (Number 2)")
+        elif number_three < 0:
+            print("\nInvalid Number, please try again! (Number 3)")
 
         else:
-            print((number_one + number_two + number_three) / 3)
+            average = (number_one + number_two + number_three) / 3
+            actual = round(average, 2)
+            print("Your average between the 3 numbers is {0}".format(actual))
 
     except Exception:
         print("Invalid Input")
